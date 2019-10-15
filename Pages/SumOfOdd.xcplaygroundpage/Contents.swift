93fbd5c0ef5c66f32ed1de3c30228794cd97d6d2
Nilay Keven <nilay.keven>
2019-10-15 14:24:06 +0300

    "rowSumOddNumbers" method is created

--- Pages/SumOfOdd.xcplaygroundpage/Contents.swift
+++ Pages/SumOfOdd.xcplaygroundpage/Contents.swift
@@ -1,7 +1,17 @@
-//: [Previous](@previous)
-
 import Foundation
+import UIKit
 
-var str = "Hello, playground"
+func rowSumOddNumbers(_ row: Int) -> Int {
+    var startPoint = row * (row - 1) + 1
+    var result = startPoint
+    for _ in 1..<row {
+        startPoint += 2
+        result += startPoint
+    }
+    return result
+}
 
-//: [Next](@next)
+func rowSumOddNumbers1(_ row: Int) -> Int {
+    return Int(pow(Double(row), 3))
+}
+print(rowSumOddNumbers1(13))
